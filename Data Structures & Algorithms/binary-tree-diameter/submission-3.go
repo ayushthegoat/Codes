54975c12ab7b315466ecf698211be8d90e37d98f
Ayush Singh <98512726+ayushthegoat@users.noreply.github.com>
2026-05-02 16:07:42 +0530

    Add: binary-tree-diameter - submission-3

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-3.go
@@ -0,0 +1,79 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+// func diameterOfBinaryTree(root *TreeNode) int {
+//     if root == nil {
+//         return 0  
+//     }
+    
+//     // Path through root
+//     throughRoot := maxDepth(root.Left) + maxDepth(root.Right)
+    
+//     // Diameter in left subtree (might be larger)
+//     leftDiameter := diameterOfBinaryTree(root.Left)
+    
+//     // Diameter in right subtree (might be larger)
+//     rightDiameter := diameterOfBinaryTree(root.Right)
+    
+//     // Return the maximum of all three
+//     return max(throughRoot, max(leftDiameter, rightDiameter))
+// }
+// func maxDepth(root *TreeNode) int {
+//    if root == nil {
+//       return 0
+//    }
+//    return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+// }
+// func diameterOfBinaryTree(root *TreeNode) int {
+//    if root == nil {
+//     return 0
+//    }
+//    d1 := depth(root.Left) + depth(root.Right)
+//    d2 := diameterOfBinaryTree(root.Left)
+//    d3 := diameterOfBinaryTree(root.Right)
+//    return max(d1, max(d2, d3))
+
+
+
+// }
+// func depth (root *TreeNode) int {
+//     if root == nil {
+//         return 0
+//     }
+//     lh := depth(root.Left)
+//     rh := depth(root.Right)
+//     return 1 + max(lh, rh)
+// }
+
+// so the intution is very simply i will fidn the height simpyl but which finding 
+// the heing i know i visint every sinlge node at each node i wil check with a
+//  golabl variable that okay sir if the heingt of the left and reight subtree
+//   from thsi node is actualy larger that what i curerntly have as the largest right ?
+var d int 
+func diameterOfBinaryTree(root *TreeNode) int {
+    d = 0
+    if root == nil {
+        return 0
+    }
+    height(root)
+    return d
+}
+
+func height(root *TreeNode) int {
+   if root == nil {
+    return 0
+   }
+   lh := height(root.Left)
+   rh := height(root.Right)
+   curH := lh + rh
+   if curH > d {
+    d = curH
+   }
+   return 1 + max(lh, rh)
+}
