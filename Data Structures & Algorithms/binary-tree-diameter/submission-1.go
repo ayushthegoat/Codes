43c638ffa21bbc53f089ed94821a174194f5ac75
Ayush Singh <98512726+ayushthegoat@users.noreply.github.com>
2026-05-02 15:53:50 +0530

    Add: binary-tree-diameter - submission-1

---
+++ Data Structures & Algorithms/binary-tree-diameter/submission-1.go
@@ -0,0 +1,52 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+// func diameterOfBinaryTree(root *TreeNode) int {
+//     if root == nil {
+//         return 0  
+//     }
+    
+//     // Path through root
+//     throughRoot := maxDepth(root.Left) + maxDepth(root.Right)
+    
+//     // Diameter in left subtree (might be larger)
+//     leftDiameter := diameterOfBinaryTree(root.Left)
+    
+//     // Diameter in right subtree (might be larger)
+//     rightDiameter := diameterOfBinaryTree(root.Right)
+    
+//     // Return the maximum of all three
+//     return max(throughRoot, max(leftDiameter, rightDiameter))
+// }
+// func maxDepth(root *TreeNode) int {
+//    if root == nil {
+//       return 0
+//    }
+//    return 1 + max(maxDepth(root.Left), maxDepth(root.Right))
+// }
+func diameterOfBinaryTree(root *TreeNode) int {
+   if root == nil {
+    return 0
+   }
+   d1 := depth(root.Left) + depth(root.Right)
+   d2 := diameterOfBinaryTree(root.Left)
+   d3 := diameterOfBinaryTree(root.Right)
+   return max(d1, max(d2, d3))
+
+
+
+}
+func depth (root *TreeNode) int {
+    if root == nil {
+        return 0
+    }
+    lh := depth(root.Left)
+    rh := depth(root.Right)
+    return 1 + max(lh, rh)
+}
