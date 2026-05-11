ad04988514af9ad5d0a668885fd8817ad4be58bd
Ayush Singh <98512726+ayushthegoat@users.noreply.github.com>
2026-05-11 09:01:56 +0530

    Add: rotate-array - submission-2

---
+++ Data Structures & Algorithms/rotate-array/submission-2.go
@@ -0,0 +1,24 @@
+func rotate(nums []int, k int) {
+   k = k % len(nums)
+   if k < 0 {
+	k = k + len(nums)
+   }
+   //part 1 reverser
+   reverse(nums, 0, len(nums) - k - 1)
+   //part 2 reverse
+   reverse(nums, len(nums) - k, len(nums) - 1)
+   //full reverse
+   reverse(nums, 0, len(nums) - 1)
+}
+func reverse(nums []int, front, rear int) {
+	i:=front
+	j:=rear
+
+	for i <= j {
+       nums[i], nums[j] = nums[j], nums[i]
+	   i++
+	   j--
+	}
+}
+
+
